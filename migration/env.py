5571f7c7ab44e5024adc5017c58ed659fd76ffe4
Ueslei Carvalho <uesleicdoliveira@gmail.com>
2021-11-19 23:57:24 -0300

    Add columns from address to clients table
    Add column for payment type in orders

--- migration/env.py
+++ migration/env.py
@@ -4,8 +4,8 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
+import src.core.models  # noqa
 from src.core.config import settings
-from src.core.models import base  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
