ccbcb47819848c78d8c166353cc61485e6d0bbdc
Ueslei Carvalho <uesleicdoliveira@gmail.com>
2021-11-20 10:51:55 -0300

    Adjust create of first superuser

--- src/core/helpers/database.py
+++ src/core/helpers/database.py
@@ -7,8 +7,9 @@ from src.monitoring import capture_exception
 from .. import controller
 from ..config import settings
 from ..constants import AccessLevel, ContextEnum
-from ..helpers.exceptions import NotFoundError
 from ..models import Context, CreateUser
+from .exceptions import NotFoundError
+from .logger import logger
 
 engine = create_engine(
     settings.SQLALCHEMY_DB_URI, connect_args={"connect_timeout": settings.SQLALCHEMY_CONNECTION_TIMEOUT}
@@ -40,14 +41,18 @@ def init_database() -> None:
             controller.user.get_by_email(session, settings.FIRST_SUPERUSER_EMAIL, context=context)
 
         except NotFoundError:
-            schema = CreateUser(
-                name=settings.FIRST_SUPERUSER_NAME,
-                email=settings.FIRST_SUPERUSER_EMAIL,
-                password=settings.FIRST_SUPERUSER_PASSWORD,
-                confirm_password=settings.FIRST_SUPERUSER_PASSWORD,
-                access_level=AccessLevel.SUPER_USER,
-            )
-            controller.user.create(session, schema=schema, context=context)
+            try:
+                schema = CreateUser(
+                    name=settings.FIRST_SUPERUSER_NAME,
+                    email=settings.FIRST_SUPERUSER_EMAIL,
+                    password=settings.FIRST_SUPERUSER_PASSWORD,
+                    confirm_password=settings.FIRST_SUPERUSER_PASSWORD,
+                    access_level=AccessLevel.SUPER_USER,
+                )
+                controller.user.create(session, schema=schema, context=context)
+
+            except Exception:
+                logger.warning("Couldn't possible create a First Superuser, maybe it alread exists?")
 
 
 def drop_database() -> None:
