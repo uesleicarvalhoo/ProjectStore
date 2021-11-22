c00bb7fe89a68336c19ff5bfd85a163cbe3fe732
Ueslei Carvalho <uesleicdoliveira@gmail.com>
2021-11-22 10:45:49 -0300

    Implementing endpoint for updateuser
    Implementing endpoint for update password

--- src/api/v1/user.py
+++ src/api/v1/user.py
@@ -9,12 +9,13 @@ from starlette.status import HTTP_201_CREATED
 from src.core.controller import user
 from src.core.helpers.database import make_session
 from src.core.models import Context, CreateUser, QueryUser, User
+from src.core.models.user import UpdateUser, UpdateUserPassword
 from src.utils.dependencies import context_manager, get_current_user
 
 router = APIRouter()
 
 
-@router.get("/me", response_model=User)
+@router.get("/me", response_model=User, response_model_exclude={"password_hash": ...})
 async def get_me(current_user: User = Depends(get_current_user)):
     return current_user
 
@@ -25,17 +26,38 @@ async def get(
     session: Session = Depends(make_session),
     context: Context = Depends(context_manager),
 ):
-    return user.get_all(session, query, context=context_manager)
+    return user.get_all(session, query, context=context)
 
 
 @router.get("/{user_id}", response_model=User, response_model_exclude={"password_hash": ...})
 async def get_by_id(
     user_id: UUID, session: Session = Depends(make_session), context: Context = Depends(context_manager)
 ):
-    return user.get_by_id(session, user_id, context=context_manager)
+    return user.get_by_id(session, user_id, context=context)
 
 
-@router.post("/", response_model=User, status_code=HTTP_201_CREATED)
-async def create(schema: CreateUser, session: Session = Depends(make_session)):
+@router.post("/", response_model=User, status_code=HTTP_201_CREATED, response_model_exclude={"password_hash": ...})
+async def create(
+    schema: CreateUser, session: Session = Depends(make_session), context: Context = Depends(context_manager)
+):
+    return user.create(session, schema, context=context)
 
-    return user.create(session, schema, context=context_manager)
+
+@router.post("/{user_id}", response_model=User, response_model_exclude={"password_hash": ...})
+async def update_by_id(
+    user_id: UUID,
+    data: UpdateUser,
+    session: Session = Depends(make_session),
+    context: Context = Depends(context_manager),
+):
+    return user.update_by_id(session, user_id, data, context)
+
+
+@router.post("/password/{user_id}")
+async def update_password_by_id(
+    user_id: UUID,
+    data: UpdateUserPassword,
+    session: Session = Depends(make_session),
+    context: Context = Depends(context_manager),
+):
+    return user.update_password(session, user_id, data, context)
