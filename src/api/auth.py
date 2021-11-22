1ce3310d433b39a42b24d6f33db025684cb02fdc
Ueslei Carvalho <uesleicdoliveira@gmail.com>
2021-11-22 10:44:43 -0300

    Implementing refresh-token endpoint

--- src/api/auth.py
+++ src/api/auth.py
@@ -4,21 +4,28 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from src.core.controller import user
 from src.core.helpers.database import Session, make_session
-from src.core.models import Context, Token
+from src.core.models import Context, Token, User
 from src.core.security import create_access_token
-from src.utils.dependencies import context_manager
+from src.utils.dependencies import context_manager, get_current_user
 
 router = APIRouter()
 
 
 @router.post("/access-token", response_model=Token)
-async def get_token(
+async def get_access_token(
     credentials: OAuth2PasswordRequestForm = Depends(),
     session: Session = Depends(make_session),
     context: Context = Depends(context_manager),
 ):
     current_user = user.authenticate(session, credentials.username, credentials.password, context=context)
 
+    token, expire = create_access_token(str(current_user.id), access_level=current_user.access_level)
+
+    return {"accessToken": token, "grantType": "bearer", "exp": expire}
+
+
+@router.post("/refresh-token", response_model=Token)
+async def refresh_access_token(current_user: User = Depends(get_current_user)):
     token = create_access_token(str(current_user.id), access_level=current_user.access_level)
 
     return {
