a5c52ef0bb7d7fdcde645dc42b0106d52d1c3fbe
Ueslei Carvalho <uesleicdoliveira@gmail.com>
2021-11-25 12:33:54 -0300

    Save accessToken on localStorage and recovery then after page refresh

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,5 +1,6 @@
 export const itemsPerPage = process.env.ITEMS_PER_PAGE || 10
 
-let envApiUrl = process.env.API_URL || `${window.location.protocol}//${window.location.host}/api`;
+// let envApiUrl = process.env.API_URL || `${window.location.protocol}//${window.location.host}/api`;
+let envApiUrl = "http://localhost:5000/api"
 
 export const apiUrl = envApiUrl;
