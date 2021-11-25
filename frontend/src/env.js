4c7c004c242d1c541ed53d7103fada673f13cb6f
Ueslei Carvalho <uesleicdoliveira@gmail.com>
2021-11-25 13:44:36 -0300

    Update Dockerfileignore
    Update Makefile
    
    Fix .env

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,6 +1,5 @@
 export const itemsPerPage = process.env.ITEMS_PER_PAGE || 10
 
-// let envApiUrl = process.env.API_URL || `${window.location.protocol}//${window.location.host}/api`;
-let envApiUrl = "http://localhost:5000/api"
+let envApiUrl = process.env.API_URL || `${window.location.protocol}//${window.location.host}/api`;
 
 export const apiUrl = envApiUrl;
